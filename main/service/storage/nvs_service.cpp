f7340e4098c388a9d8aa9278c7eae2de40b0d78f
DESKTOP\eugen <eugenetham1994@gmail.com>
2026-07-09 20:40:31 +0800

    chore: update

--- main/service/storage/nvs_service.cpp
+++ main/service/storage/nvs_service.cpp
@@ -2,48 +2,107 @@
 // Created by eugen on 7/5/2026.
 //
 
-#include <memory>
-#include <span>
+#include "nvs_service.hpp"
 
-#include "esp_err.h"
-#include "esp_system.h"
-#include "nvs.h"
-#include "nvs_handle.hpp"
-#include <etl/span.h>
-#include <etl/string_view.h>
+#include <cstring>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <etl/string.h>
+#include <nvs.h>
+#include <nvs_handle.hpp>
 
-class NvsService {
-public:
-	NvsService() = default;
+using etl::tuple;
+using svc::storage::NvsService;
 
-	/**
-	 *
-	 * @param ns_name
-	 * @param key
-	 * @param out
-	 * @return esp_err_t, which can be of values ESP_OK, ESP_ERR_NVS_NOT_FOUND, or other error codes.
-	 */
-	template <typename T>
-	std::tuple<bool, esp_err_t> get_item(etl::span<T> out, etl::string_view ns_name, etl::string_view key) {
-		esp_err_t err = ESP_OK;
-		// Handle will automatically close when going out of scope or when it's reset.
-		std::unique_ptr<nvs::NVSHandle> handle = nvs::open_nvs_handle(ns_name.data(), NVS_READWRITE, &err);
+namespace {
+constexpr const char *TAG = "NvsService";
+}
 
-		if (err != ESP_OK) {
-			printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-			return {false, err};
-		}
+void NvsService::to_cstr(etl::string_view sv, char *out, size_t out_size) {
+	size_t len = sv.size() < (out_size - 1) ? sv.size() : (out_size - 1);
+	std::memcpy(out, sv.data(), len);
+	out[len] = '\0';
+}
 
-		printf("Done\n");
-		err = handle->get_blob(key.data(), out.data(), out.size());
-		switch (err) {
-		case ESP_OK:
-			return {true, ESP_OK};
-		case ESP_ERR_NVS_NOT_FOUND:
-			return {false, ESP_OK};
-		default:
-			printf("err not handled: %d", err);
-		}
-		return {false, err};
+tuple<size_t, esp_err_t> NvsService::get_item_size(etl::string_view ns_name,
+												   etl::string_view key) {
+	char ns_buf[kMaxNameLen];
+	char key_buf[kMaxNameLen];
+	to_cstr(ns_name, ns_buf, sizeof(ns_buf));
+	to_cstr(key, key_buf, sizeof(key_buf));
+
+	esp_err_t err;
+	std::unique_ptr<nvs::NVSHandle> handle =
+		nvs::open_nvs_handle(ns_buf, NVS_READONLY, &err);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to open namespace '%s' (%s)", ns_buf,
+				 esp_err_to_name(err));
+		return {0, err};
 	}
-};
+
+	size_t required_size = 0;
+	err = handle->get_item_size(nvs::ItemType::BLOB, key_buf, required_size);
+	if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND) {
+		ESP_LOGE(TAG, "Failed to get size of '%s' (%s)", key_buf,
+				 esp_err_to_name(err));
+	}
+
+	return {required_size, err};
+}
+
+esp_err_t NvsService::get_blob(etl::string_view ns_name, etl::string_view key,
+							   void *out, size_t required_size) {
+	char ns_buf[kMaxNameLen];
+	char key_buf[kMaxNameLen];
+	to_cstr(ns_name, ns_buf, sizeof(ns_buf));
+	to_cstr(key, key_buf, sizeof(key_buf));
+
+	esp_err_t err;
+	std::unique_ptr<nvs::NVSHandle> handle =
+		nvs::open_nvs_handle(ns_buf, NVS_READONLY, &err);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to open namespace '%s' (%s)", ns_buf,
+				 esp_err_to_name(err));
+		return err;
+	}
+
+	err = handle->get_blob(key_buf, out, required_size);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to read blob '%s' (%s)", key_buf,
+				 esp_err_to_name(err));
+	}
+
+	return err;
+}
+
+esp_err_t NvsService::write_blob(etl::string_view ns_name, etl::string_view key,
+								 void *data, size_t size) {
+	char ns_buf[kMaxNameLen];
+	char key_buf[kMaxNameLen];
+	to_cstr(ns_name, ns_buf, sizeof(ns_buf));
+	to_cstr(key, key_buf, sizeof(key_buf));
+
+	esp_err_t err;
+	std::unique_ptr<nvs::NVSHandle> handle =
+		nvs::open_nvs_handle(ns_buf, NVS_READWRITE, &err);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to open namespace '%s' (%s)", ns_buf,
+				 esp_err_to_name(err));
+		return err;
+	}
+
+	err = handle->set_blob(key_buf, data, size);
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to write blob '%s' (%s)", key_buf,
+				 esp_err_to_name(err));
+		return err;
+	}
+
+	err = handle->commit();
+	if (err != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to commit NVS changes for '%s' (%s)", key_buf,
+				 esp_err_to_name(err));
+	}
+
+	return err;
+}
--- main/service/storage/nvs_service.hpp
+++ main/service/storage/nvs_service.hpp
@@ -5,5 +5,33 @@
 #ifndef CPP_STARTER_NVS_SERVICE_H
 #define CPP_STARTER_NVS_SERVICE_H
 
+#include "domain/interface/invs_service.hpp"
+#include <esp_err.h>
+#include <etl/string_view.h>
+
+namespace svc::storage {
+// https://github.com/espressif/esp-idf/blob/master/examples/storage/nvs/nvs_rw_value_cxx/main/nvs_value_example_main.cpp
+class NvsService : public INvsService {
+public:
+	NvsService() = default;
+
+	etl::tuple<size_t, esp_err_t> get_item_size(etl::string_view ns_name,
+												etl::string_view key);
+	esp_err_t get_blob(etl::string_view ns_name, etl::string_view key,
+					   void *out, size_t required_size);
+	esp_err_t write_blob(etl::string_view ns_name, etl::string_view key,
+						 void *data, size_t size);
+
+private:
+	// NVS namespace and key names are limited to 15 characters + null
+	// terminator by the underlying NVS implementation.
+	static constexpr size_t kMaxNameLen = 16;
+
+	// Copies (truncates if necessary) an etl::string_view into a
+	// null-terminated buffer suitable for the C/C++ NVS APIs.
+	static void to_cstr(etl::string_view sv, char *out, size_t out_size);
+};
+
+} // namespace svc::storage
 
 #endif // CPP_STARTER_NVS_SERVICE_H
