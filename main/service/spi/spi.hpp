f643eb5233f131788720aad545625839ad3b3f81
DESKTOP\eugen <eugenetham1994@gmail.com>
2026-09-03 18:35:10 +0800

    chore: spi

--- main/service/spi/spi.hpp
+++ main/service/spi/spi.hpp
@@ -4,21 +4,29 @@
 
 #ifndef CPP_STARTER_SPI_HPP
 #define CPP_STARTER_SPI_HPP
+#include "domain/interface/ispi_service.hpp"
 #include <driver/spi_master.h>
 
-#define GPIO_MISO  19
-#define GPIO_MOSI  23
-#define GPIO_SCLK   18
-#define PIN_NUM_CS    5
+#define GPIO_MISO 19
+#define GPIO_MOSI 23
+#define GPIO_SCLK 18
+#define PIN_NUM_CS 5
 
 namespace svc::spi {
-class SPIService {
+class SPIService : public ISPIService {
 private:
 	spi_device_handle_t _spi;
+
 public:
 	SPIService();
-	~SPIService();
-	esp_err_t subscribe(spi_device_handle_t spi, spi_device_interface_config_t devcfg);
+	~SPIService() override;
+	spi_device_interface_config_t
+	create_default_device_config(uint8_t pin_gpio) override;
+	esp_err_t subscribe(spi_device_handle_t spi,
+						spi_device_interface_config_t devcfg) override;
+	esp_err_t unsubscribe(spi_device_handle_t spi) override;
+	esp_err_t spi_read_write_byte(uint8_t *rx_data, const uint8_t *tx_data,
+								  size_t bit_size) const override;
 };
-}
+} // namespace svc::spi
 #endif // CPP_STARTER_SPI_HPP
